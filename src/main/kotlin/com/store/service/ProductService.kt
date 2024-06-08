b3221a0e1b76b75dbb03f63ffb41bfa62ca36811
Mounika <kurapati.mounika@johndeere.com>
2024-06-08 17:24:49 +0530

    Add products endpoint
    Dummy commit to trigger pipeline

---
+++ src/main/kotlin/com/store/service/ProductService.kt
@@ -0,0 +1,36 @@
+package com.store.service
+
+import com.store.models.Product
+import com.store.models.ProductDetails
+import com.store.models.ProductType
+import com.store.repository.ProductRepository
+
+class ProductService {
+
+    private val productRepository: ProductRepository = ProductRepository();
+
+    fun validateProductDetails(productDetails: ProductDetails): String? {
+        if (productDetails.name.isEmpty() ||
+            productDetails.name.toBooleanStrictOrNull() != null
+            || productDetails.name.toDoubleOrNull() != null
+        ) {
+            return "Product name is required"
+        }
+        if (productDetails.inventory == null) {
+            return "Inventory is required"
+        }
+        if (productDetails.inventory < 0) {
+            return "Inventory cannot be negative"
+        }
+        return null
+    }
+
+    fun add(productDetails: ProductDetails): Int {
+        return productRepository.add(productDetails)
+    }
+
+    fun getProductBasedOnType(type: ProductType?): List<Product> {
+    return productRepository.all.filter { it.type == type }
+    }
+
+}
